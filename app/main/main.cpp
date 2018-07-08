4ecbae88129a5cb2d69b93fe3d6ff1b989201cd9
Lukas Tolksdorf <Lukas.tolksdorf@tum.de>
2018-07-08 22:43:27 +0200

    prototyp of Slice class

--- app/main/main.cpp
+++ app/main/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "matrix.h"
 
+using Matr::Slice;
 
 int main(int argc, char* argv[])
 {
-    Matr::Slice<int> Slice1 = Matr::Slice<int>();
+    Matr::Slice<2> test1 =  Matr::Slice<2>();
+    Matr::Slice<1> test2 = Matr::Slice<1>();
 
-    std::cout << "Hello World!\n";
+    double TestArray[2] = { 1.f, 0.1f };
+    test1 = TestArray;
+    std::cout << test1.Values_[1] <<"\n";
     return 0;
 }
