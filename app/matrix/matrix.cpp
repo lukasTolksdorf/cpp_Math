1e13f55d10d640622830a3c1351b554787f3301a
Lukas Tolksdorf <Lukas.tolksdorf@tum.de>
2018-07-08 22:43:27 +0200

    prototyp of Slice class

--- app/matrix/matrix.cpp
+++
@@ -1,6 +0,0 @@
-#include "matrix.h"
-
-Matr::Slice::Slice(int size) :
-	Size_(size)
-{
-}
--- app/matrix/matrix.h
+++ app/matrix/matrix.h
@@ -1,22 +1,33 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
+namespace Matr {
 
-namespace Matr{
 
-template <typename Type>
+template <int size>
 class Slice
 {
 public:
-	Slice();
+    Slice() {};
+    Slice(double values[size]) :
+        Values_(values)
+    {
+        //intentionally left empty
+    }
 
+    Slice& operator=(double rhs[size])
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            Values_[i] = rhs[i];
+        }
+        return *this;
+    }
+    double Values_[size];
 private:
 
-	int Size_;
-	Type Values_[];
+    int Size_ = size;
 };
-
-
 
 } /// namespace matr
 
