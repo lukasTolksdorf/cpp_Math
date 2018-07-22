0f052e9a9db20919487cec28608ff58e7a8a3a13
Lukas Tolksdorf <Lukas.tolksdorf@tum.de>
2018-07-22 20:10:55 +0200

    added function framework for operator*

--- app/main/main.cpp
+++ app/main/main.cpp
@@ -5,5 +5,11 @@
 
 int main(int argc, char* argv[])
 {
-
+    Matr::Matrix<1, 2> Matr1;
+    Matr1.SetZeros();
+    Matr::Matrix<2, 3> Matr2;
+    Matr2.SetZeros();
+    auto Matr3 = Matr1 * Matr2;
+    Matr3.SetZeros();
+    Matr3.writeToIostream();
 }
--- app/matrix/matrix.h
+++ app/matrix/matrix.h
@@ -32,34 +32,55 @@ public:
     }
 
     void SetZeros() {
-        for(int i; i<Tsize0; ++i) {
-            for(int j; j<Tsize1; ++j) {
-                Values_[i][j] = 0;
+        for(int i=0; i<Tsize0; ++i) {
+            for(int j=0; j<Tsize1; ++j) {
+                Values_[i][j] = (double)rand() / RAND_MAX;;
             }
         }
     }
 
-    double* GetRow(int idx1){
-         return &Values_[idx1][0];
-     }
+    double* GetRow(int idx1) {
+        return &Values_[idx1][0];
+    }
 
-    double* operator[](int idx1){
+    double* operator[](int idx1) {
         return this->GetRow(idx1);
     }
 
+    void writeToIostream()
+    {
+        std::cout << Values_[0][0] << "\n";
+    }
     //multiplication zahl, matrix
 
     //multiplication matrix, matrix
 
+    //template<uint16 dim1>
+    //Matrix<Tsize0, dim1>& operator*(Matrix<uint16, uint16 dim1>)
+    //{
+
+    //}
     //addition matrix, matrix
 
 
 
-
 private:
+    template <uint16 outer1, uint16 inner, uint16 outer2>
+    friend Matrix<outer1, outer2> operator*(const Matrix<outer1, inner>& lhs,
+                                            const Matrix<inner, outer2>& rhs);
 
     double Values_[Tsize0][Tsize1];
-};
+}; //Matrix
+
+
+
+template <uint16 outer1, uint16 inner, uint16 outer2>
+Matrix<outer1, outer2> operator*(const Matrix<outer1, inner>& lhs,
+                                 const Matrix<inner, outer2>& rhs)
+{
+
+    return Matr;
+}
 
 } /// namespace matr
 
